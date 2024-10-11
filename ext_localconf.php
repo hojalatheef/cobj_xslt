f15145d98617394ed8aedceb17b778d3a1d5d186
Hoja Mustaffa Abdul Latheef <hlatheef@jweiland.net>
2024-10-11 10:59:29 +0200

    [TASK] Fixed version constraints

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,10 +1,13 @@
 <?php
-if (!defined('TYPO3_MODE')) die('Not in Typo3');
+
+if (!defined('TYPO3')) {
+    die('Access denied.');
+}
 
 // defines content object XSLT
-$GLOBALS['TYPO3_CONF_VARS']['FE']['ContentObjects'] = array_merge($GLOBALS['TYPO3_CONF_VARS']['FE']['ContentObjects'], [
-    'XSLT' =>  Digicademy\CobjXslt\ContentObject\XsltContentObject::class
-]);
+//$GLOBALS['TYPO3_CONF_VARS']['FE']['ContentObjects'] = array_merge($GLOBALS['TYPO3_CONF_VARS']['FE']['ContentObjects'], [
+//    'XSLT' =>  Digicademy\CobjXslt\ContentObject\XsltContentObject::class
+//]);
 
 // define example RTE preset for XSLT TypoTag
 $GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['cobj_xslt'] = 'EXT:cobj_xslt/Configuration/RTE/Default.yaml';
